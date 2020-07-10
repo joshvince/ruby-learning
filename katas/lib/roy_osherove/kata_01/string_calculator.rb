d1be1ac8e1ac502f505ee9114ea38a129935e61e
joshvince <joshmvince@gmail.com>
2020-07-10 15:54:30 +0100

    refactor based on panos's feedback

--- katas/lib/roy_osherove/kata_01/string_calculator.rb
+++ katas/lib/roy_osherove/kata_01/string_calculator.rb
@@ -1,60 +1,58 @@
 module RoyOsherove
   module Kata01
     class StringCalculator
-      attr_accessor :input, :delimiter, :sequence
+      MULTI_CHAR_SIGNATURE = %r{\[(\S+?)\]}
 
       def add(string)
         @input = string
         
-        return 0 if input.length == 0
+        return 0 if input.empty?
         
-        configure_delimiter
-        integers = sequence.split(delimiter).filter { |i| i != "" }.map { |s| s.to_i }
-        valid_integers(integers).sum
+        setup_sequence_and_delimiter
+        @sequence = sequence.split(delimiter).map(&:to_i)
+        raise_if_negatives
+        reject_over_one_thousand.sum
       end
-      
-      private
 
-      def configure_delimiter
+      private
+      
+      attr_accessor :input, :delimiter, :sequence
+
+      def setup_sequence_and_delimiter
         if input.start_with? "//"
-          handle_custom_delimiter
+          delim_config, @sequence = input.split(/\n/, 2)
+          @delimiter = handle_custom_delimiter(delim_config)
         else
           # the whole input is the sequence if the input didn't define a custom delimiter
           @sequence = input
-          @delimiter = /[,]|[\n]/
+          @delimiter = /,|\n/
         end
       end
 
-      def handle_custom_delimiter
-        delim_config, @sequence = input.split(/\n/, 2)
-
-        multi_char_signature = %r{\[(\S+?)\]}
-
-        case delim_config.match multi_char_signature
-        when nil
-          # the spec says that anything not enclosed in square brackets is valid as a single char delimiter
-          @delimiter = delim_config.chars.last
+      def handle_custom_delimiter(config)
+        if config.match MULTI_CHAR_SIGNATURE
+          build_delimiter(config)
         else
-          build_delimiter(delim_config, multi_char_signature)
+          # the spec says that anything not enclosed in square brackets is valid as a single char delimiter
+          config.chars.last
         end
       end
 
-      def build_delimiter(input, regex)
-        inputted_chars = input.scan(regex).flatten
+      def build_delimiter(input)
+        inputted_chars = input.scan(MULTI_CHAR_SIGNATURE).flatten
         capture_groups = inputted_chars.map { |char| "(" + Regexp.quote(char) + "+?)" }
         regex_string = "\[" + capture_groups.join("|") + "\]"
         
-        @delimiter = Regexp.new(regex_string)
+        Regexp.new(regex_string)
       end
 
-      def valid_integers(integers)
-        raise_negative_number_error(integers) unless integers.all? {|i| i.positive?}
-        
-        integers.reject { |i| i > 1000 }
+      def raise_if_negatives
+        negatives = sequence.filter(&:negative?)
+        raise "negatives not allowed, recieved #{negatives}" unless negatives.empty?
       end
 
-      def raise_negative_number_error(integers)
-        raise "negatives not allowed, received: #{integers.filter { |n| n.negative? }}"
+      def reject_over_one_thousand
+        sequence.reject { |i| i > 1000 }
       end
     end
   end
